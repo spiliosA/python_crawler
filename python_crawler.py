3fd79f2b497bbc7ba01f7a0a614cfd8b565c4f74
Spilios Alexopoulos <salexopo@lp01616.openbet>
2015-07-31 12:31:42 +0100

    Adding comments...

--- python_crawler.py
+++ python_crawler.py
@@ -5,32 +5,40 @@ import urllib
 import sys  
 import pprint
 
+# set encoding
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
+# initialise gloabal variables
 base_url      = 'http://wiprodigital.com/'
 parsed_urls   = []
-path          = {}
-images        = {}
+sitemap       = {}
+internal_urls = {}
 external_urls = {}
+images        = {}
+
 
 # create a subclass and override the handler methods
+# now generates 2 lists for links and images that exist in page
 #
 class MyHTMLParser(HTMLParser):
 
 	l_link = []
 	l_img  = []
 
-	def handle_starttag(self, tag, attrs, path=None):
+	def handle_starttag(self, tag, attrs):
 		if tag == 'a' :
 			attrs   = dict(attrs)
 			self.l_link.append(attrs.get("href", ""))
 		if tag == 'img' :
 			attrs   = dict(attrs)
+			# do not list the same images twice
 			if attrs.get("src", "") not in self.l_img:
 				self.l_img.append(attrs.get("src", ""))
 
 
+# parses a url, populate the images and external_urls lists and return internal url list
+#
 def get_links(url):
 	global images
 	global external_urls
@@ -43,15 +51,18 @@ def get_links(url):
 	data =     response.read()
 	parser.feed(data)
 	
-	urls          = get_internal_url(parser.l_link)
-	internal_urls = urls[0]
+	urls          = split_urls(parser.l_link)
 
 	external_urls[url] = urls[1]
 	images[url]        = parser.l_img
 
-	return internal_urls
+	# return the internal urls
+	return urls[0]
 
-def get_internal_url(url_list) :
+
+# split urls to internal and external
+#
+def split_urls(url_list) :
 	internal_list = []
 	external_list = []
 
@@ -63,30 +74,38 @@ def get_internal_url(url_list) :
 
 	return [internal_list,external_list]
 
+
+# parse recursively through urls
+#
 def parse(url) :
 
 	for child_url in get_links(url) :
 		# exclude parent-child combinations in order to avoid infinite loop
 		if [url,child_url] not in parsed_urls :
 			parsed_urls.append([url,child_url])
-		#if url not in parsed_urls :
-			#parsed_urls.append(url)
 
-			if url not in path:
-				path[url] = list()
-			path[url].append(child_url)
+			if url not in internal_urls:
+				internal_urls[url] = list()
+			internal_urls[url].append(child_url)
 			parse(child_url)
 
 
-sitemap = {}
-root = 'privacy-policy'
+# print the output
+#
+def print_map() :
+	for key in internal_urls.keys():
+		if key not in sitemap:
+			sitemap[key] = {}
+		sitemap[key]['internal_links'] = internal_urls[key]
+		sitemap[key]['external_links'] = external_urls[key]
+		sitemap[key]['images']         = images[key]
+
+	pprint.pprint(sitemap)	
+
+
+# main call
+#
+root = ''
 parse(root)
 
-for key in path.keys():
-	if key not in sitemap:
-		sitemap[key] = {}
-	sitemap[key]['internal_links'] = path[key]
-	sitemap[key]['external_links'] = external_urls[key]
-	sitemap[key]['images']         = images[key]
 
-pprint.pprint(sitemap)
